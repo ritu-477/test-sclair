969e215c20d0e42a6eb56d64814ee9ab121b6a04
ritu-477 <ritujoharbishnoi@gmail.com>
2024-06-07 19:22:25 +0530

    roadmap done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,22 @@ function openNav() {
     document.body.classList.toggle("overflowhidden")
     document.querySelector(".iconbox").classList.toggle("cross")
 }
+// scroll-to -top-button////
+const mybutton = document.querySelector(".topBtn");
+const show = document.querySelector(".show")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 600) {
+        mybutton.classList.add("show")
+    }
+    else {
+        mybutton.classList.remove("show")
+    }
+})
+mybutton.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+
+});
 
 
 $('.responsive_2').slick({
